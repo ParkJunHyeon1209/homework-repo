57656a9df35f802370f84365316fcfc64b3f9be2
“ParkJunHyeon1209” <pjh140123@gmail.com>
2026-02-03 17:24:55 +0900

    hoppies 배열에 문자열 분리 및 나이 구하기 수정

--- 2026-02-03/1-2-2/src/components/Card.jsx
+++ 2026-02-03/1-2-2/src/components/Card.jsx
@@ -1,12 +1,13 @@
 export default function Card() {
   const name = "박준현";
   const birthYear = 2000;
-  const hobbies = ["노래방, 소설읽기, 뒹굴거리기"];
+  const hobbies = ["노래방", "소설읽기", "뒹굴거리기"];
+  const currentYear = new Date().getFullYear();
   return (
     <div className="card-wrap" style={{ boxShadow: "0 0 15px #db76e1" }}>
       <h1 className="name">{name}</h1>
       <p className="age" style={{ color: "red" }}>
-        나이 : {2026 - birthYear + 1}
+        나이 : {currentYear - birthYear + 1}
       </p>
       <p className="hobby">취미 : {hobbies.join(", ")}</p>
       <p className="mbti" style={{ fontWeight: 600 }}>
