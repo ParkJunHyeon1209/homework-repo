4a687a37c8b5c56a96d9b45391820cfa30cfabdc
“ParkJunHyeon1209” <pjh140123@gmail.com>
2026-03-04 17:04:11 +0900

    2026-03-04 homework done

---
+++ 2026-03-04/5-1-3/types.ts
@@ -0,0 +1,18 @@
+let myName: string = "박준현";
+let age: number = 25;
+let isGraduate: boolean = true;
+let lecture: string[] = ["프론트엔드 부트캠프"];
+
+type Point3D = [x: number, y: number, z: number];
+
+let role: "admin" | "manager" | "user";
+
+let SubjectScores: Record<string, number>;
+
+type BasicUser = { name: string; age: number };
+type Contact = { email: string; tel: string };
+type EmployeeProfile = BasicUser & Contact;
+
+const evaluateEmployee = (obj: EmployeeProfile): "해고됨" | "승진함" => {
+  return "해고됨";
+};
